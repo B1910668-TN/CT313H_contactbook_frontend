aafe5763e2b3811adcf4faa1d6170c39bf2db8c1
B1910668-TN <ngab1910668@student.ctu.edu.vn>
2022-10-28 15:18:49 +0700

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2022: true,
+    },
+    extends: 'eslint:recommended',
+    rules: {
+        'vue/no-reserved-component-names': 'off',    },
+};
